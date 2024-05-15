a1792d177d876cf75db8d6c2cdf4ab9c4572bb53
Arthur Liu <arthur.liu.lsh@protonmail.com>
2024-05-15 14:36:55 +0200

    feat: added comment and raising error in Python

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import rust_interop
 rust_interop.py_print()
 print(rust_interop.py_add(1, 2))
+try:
+    rust_interop.py_fail(True)
+except ValueError as e:
+    print(f"Failed as expected: '{e}'")
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 use pyo3::prelude::*;
+use pyo3::exceptions::PyValueError;
+
+// C part
 
 #[no_mangle]
 pub extern "C" fn rs_print() {
@@ -11,6 +14,9 @@ pub extern "C" fn rs_add(a: i32, b: i32) -> i32
     a + b
 }
 
+// Python part
+// Functions need to be prefixed with #[pyfunction], and encapsulate the result in PyResult
+
 #[pyfunction]
 fn py_print() {
     println!("Goodbye world...");
@@ -21,9 +27,20 @@ fn py_add(a: i32, b: i32) -> PyResult<i32> {
     Ok(a + b)
 }
 
+#[pyfunction]
+fn py_fail(b: bool) -> PyResult<()> {
+    if b {
+        Err(PyValueError::new_err("rust method error"))
+    } else {
+        Ok(())
+    }
+}
+
+// Each function must explicitly be added here. The name of the function is the name of the module
 #[pymodule]
 fn rust_interop(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(py_print, m)?)?;
+    m.add_function(wrap_pyfunction!(py_fail, m)?)?;
     m.add_function(wrap_pyfunction!(py_add, m)?)?;
     Ok(())
 }
