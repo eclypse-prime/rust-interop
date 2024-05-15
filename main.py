24c64690fa086eed9fafd121e685a4fb5e32d6d6
Arthur Liu <arthur.liu.lsh@protonmail.com>
2024-05-15 11:16:28 +0200

    feat: added Python interop code

---
+++ main.py
@@ -0,0 +1,3 @@
+import rust_interop
+rust_interop.py_print()
+print(rust_interop.py_add(1, 2))
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use pyo3::prelude::*;
+
 #[no_mangle]
 pub extern "C" fn rs_print() {
     println!("Hello world!");
@@ -7,4 +9,21 @@ pub extern "C" fn rs_print() {
 pub extern "C" fn rs_add(a: i32, b: i32) -> i32
 {
     a + b
+}
+
+#[pyfunction]
+fn py_print() {
+    println!("Goodbye world...");
+}
+
+#[pyfunction]
+fn py_add(a: i32, b: i32) -> PyResult<i32> {
+    Ok(a + b)
+}
+
+#[pymodule]
+fn rust_interop(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(py_print, m)?)?;
+    m.add_function(wrap_pyfunction!(py_add, m)?)?;
+    Ok(())
 }
